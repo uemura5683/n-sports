0d90e9ec57e2a9cd416f64b43268462fff0a6014
uemu5683 <nobuyoshi5683@gmail.com>
2023-10-29 00:15:23 +0900

    ファイル生成 設定など

---
+++ library/microcms.ts
@@ -0,0 +1,42 @@
+// SDK利用準備
+import { createClient, MicroCMSQueries } from "microcms-js-sdk";
+
+const nu_client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN_NU,
+  apiKey: import.meta.env.MICROCMS_API_KEY_NU,
+});
+
+// 型定義
+export type Blog = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+};
+
+export type BlogResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Blog[];
+};
+
+
+
+// nu-blogの呼び出し
+export const getNublog = async (queries?: MicroCMSQueries) => {
+  return await nu_client.get<BlogResponse>({ endpoint: "all", queries });
+};
+export const getNublogDetai = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await nu_client.getListDetail<Blog>({
+    endpoint: "all",
+    contentId,
+    queries,
+  });
+};
