53d4db13a42de9f44ec2999af86753c2ddcb0bdf
uemu5683 <nobuyoshi5683@gmail.com>
2023-10-30 00:02:36 +0900

    サイト改修

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: "jit",
+    prefix: "tw-",
+    content: [
+      "src/components/*.astro",
+      "src/layouts/*.astro",
+      "src/pages/*.astro",
+      "src/pages/*/*.astro",
+    ],
+    theme: {
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        '100%': '100%',
+      },
+      extend: {
+        screens: {
+          'sm': {'min': '768px'},
+          'md': {'min': '980px'},
+          'lg': {'min': '1200px'},
+          'sm-max': {'max': '767px'},
+          'md-max': {'max': '979px'},
+          'lg-max': {'max': '1199px'}
+        },
+      },
+    },
+    plugins: [],
+  }
